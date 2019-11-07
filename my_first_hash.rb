1cedad5f91871fb5c29aac3b6d256f5def44741e
Wills Blake <richwblake@gmail.com>
2019-11-07 02:05:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,9 +10,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   item_hash = {
-    "whale_bone_corsets" => 5,
+    "whale bone corsets" => 5,
     "porcelain vases" => 2,
-    "oil_paintings" => 3
+    "oil paintings" => 3
   }
 
 
